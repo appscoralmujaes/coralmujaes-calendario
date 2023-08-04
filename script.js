3e2e006d514a7665dcdcdc236410b702d7efaa66
Franco Garzon <franco.garzon@mediamonks.com>
2023-08-04 19:47:59 -0300

    Add fake calendar

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-    $("#calendar").fullCalendar();
+$(document).ready(function () {
+  $("#calendar").fullCalendar({
+    header: {
+      left: "title",
+      center: "",
+      right: "today",
+    },
+  });
+});
+$(document).ready(function () {
+  $(".fc-day-number").on("click", function (event) {
+    event.preventDefault();
+    event.target.classList.contains("focused-element")
+      ? $(event.target).removeClass("focused-element")
+      : $(event.target).addClass("focused-element");
+
+    console.log(event.target, "onclick");
+    console.log("Dia seleccionado");
+  });
 });
